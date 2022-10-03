1c9bc54b1e5958212ec728da30faf39649e805ee
l3modz <l3modz@gmail.com>
2022-10-03 10:37:11 +0700

    Update automatic_logs_LINUX_SERVER.sh

--- automatic_logs_LINUX_SERVER.sh
+++ automatic_logs_LINUX_SERVER.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright under GNU General Public License 3.0
+# Copyright under GNU General Public License v3.0
 
 cat /var/log/boot.log > /var/www/html/logmadeinalone/01.boot.txt
 cat /var/log/cron > /var/www/html/logmadeinalone/02.cron.txt
@@ -10,7 +10,9 @@ cat /var/log/messages > /var/www/html/logmadeinalone/06.messages.txt
 cat /var/log/secure > /var/www/html/logmadeinalone/07.secure.txt
 cat /var/log/yum.log > /var/www/html/logmadeinalone/08.yum.txt
 cat /var/log/httpd/access_log > /var/www/html/logmadeinalone/09.httpd_access.txt
+
 cat /var/log/httpd/error_log > /var/www/html/logmadeinalone/10.httpd_error.txt
 
 #password logmadeinalone folder of http/html using .htpasswd
 
+# ^-^ ^-^ ^-^
