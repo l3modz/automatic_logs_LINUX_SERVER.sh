ab6d7ca442d5855a5b629e28dec54f6249ec6bf0
l3modz <l3modz@gmail.com>
2025-12-09 18:51:53 +0700

    Update automatic_logs_LINUX_SERVER.sh
    change loginalone to logwebdesktoplite

--- automatic_logs_LINUX_SERVER.sh
+++ automatic_logs_LINUX_SERVER.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 # Copyright under GNU General Public License v3.0
 
-cat /var/log/boot.log > /var/www/html/logmadeinalone/01.boot.txt
-cat /var/log/cron > /var/www/html/logmadeinalone/02.cron.txt
-cat /var/log/dmesg > /var/www/html/logmadeinalone/03.dmesg.txt
-cat /var/log/firewalld > /var/www/html/logmadeinalone/04.firewalld.txt
-cat /var/log/lastlog > /var/www/html/logmadeinalone/05.lastlog.txt
-cat /var/log/messages > /var/www/html/logmadeinalone/06.messages.txt
-cat /var/log/secure > /var/www/html/logmadeinalone/07.secure.txt
-cat /var/log/yum.log > /var/www/html/logmadeinalone/08.yum.txt
-cat /var/log/httpd/access_log > /var/www/html/logmadeinalone/09.httpd_access.txt
+cat /var/log/boot.log > /var/www/html/logwebdesktoplite/01.boot.txt
+cat /var/log/cron > /var/www/html/logwebdesktoplite/02.cron.txt
+cat /var/log/dmesg > /var/www/html/logwebdesktoplite/03.dmesg.txt
+cat /var/log/firewalld > /var/www/html/logwebdesktoplite/04.firewalld.txt
+cat /var/log/lastlog > /var/www/html/logwebdesktoplite/05.lastlog.txt
+cat /var/log/messages > /var/www/html/logwebdesktoplite/06.messages.txt
+cat /var/log/secure > /var/www/html/logwebdesktoplite/07.secure.txt
+cat /var/log/yum.log > /var/www/html/logwebdesktoplite/08.yum.txt
+cat /var/log/httpd/access_log > /var/www/html/logwebdesktoplite/09.httpd_access.txt
 
-cat /var/log/httpd/error_log > /var/www/html/logmadeinalone/10.httpd_error.txt
+cat /var/log/httpd/error_log > /var/www/html/logwebdesktoplite/10.httpd_error.txt
 
-#password logmadeinalone folder of http/html using .htpasswd
+#password your logwebdesktoplite folder of http/html using .htpasswd
 
 # ^-^ ^-^ ^-^
